dffe708bdc51798883fc4c3f3be38c3207c5d0ae
LouisC-1952673 <louis.caerts@student.uhasselt.be>
2025-05-14 22:22:29 +0200

    Pop up test (+ bottom right position)
    * App can be programmatically popped up when it is closed
    * App will always spawn in the bottom right corner of the screen

--- electron-app/main.js
+++ electron-app/main.js
@@ -1,4 +1,4 @@
-const { app, BrowserWindow, Tray, Menu } = require('electron/main')
+const { app, BrowserWindow, Tray, Menu, screen } = require('electron/main')
 const path = require('path');
 const { startActivityWatch, getBuckets } = require('./aw');
 
@@ -6,9 +6,20 @@ let win;
 let tray = null;
 
 const createWindow = () => {
+  const primaryDisplay = screen.getPrimaryDisplay();
+  const { width: screenWidth, height: screenHeight } = primaryDisplay.workAreaSize;
+
+  const windowWidth = 300;
+  const windowHeight = 400;
+
+  const x = screenWidth - windowWidth;
+  const y = screenHeight - windowHeight;
+
   win = new BrowserWindow({
-    width: 800,
-    height: 600,
+    width: windowWidth,
+    height: windowHeight,
+    x: x,
+    y: y,
     alwaysOnTop: true
   })
 
@@ -42,8 +53,30 @@ const createWindow = () => {
 
   // Optional: Reopen app with double-click on the tray icon
   tray.on('double-click', () => {
+    const { width: screenWidth, height: screenHeight } = screen.getPrimaryDisplay().workAreaSize;
+    const [windowWidth, windowHeight] = win.getSize();
+    const x = screenWidth - windowWidth;
+    const y = screenHeight - windowHeight;
+    win.setBounds({ x, y, width: windowWidth, height: windowHeight });
+
+    if (win.isMinimized()) win.restore();
     win.show();
+    win.focus();
   });
+}
+
+function bringBackApp() {
+  if (win) {
+    const { width: screenWidth, height: screenHeight } = screen.getPrimaryDisplay().workAreaSize;
+    const [windowWidth, windowHeight] = win.getSize();
+    const x = screenWidth - windowWidth;
+    const y = screenHeight - windowHeight;
+    win.setBounds({ x, y, width: windowWidth, height: windowHeight });
+
+    if (win.isMinimized()) win.restore();
+    win.show();
+    win.focus();
+  }
 }
 
 getBuckets().then(buckets => console.log(buckets));
@@ -51,6 +84,14 @@ getBuckets().then(buckets => console.log(buckets));
 app.whenReady().then(() => {
   startActivityWatch();
   createWindow();
+
+  // Simulate an automatic popup after 5 seconds (pop up test)
+  /*
+  setTimeout(() => {
+    console.log('🔔 Bringing app back up!');
+    bringBackApp();
+  }, 10000);
+  */
 });
 
 // Ensures app stays running even with no windows visible
